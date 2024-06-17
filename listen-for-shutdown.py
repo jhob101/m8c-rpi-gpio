f2c5d344115a88cedcb33bd4323d2c118891c916
John Hobson <3026384+jhob101@users.noreply.github.com>
2024-06-17 11:53:01 +0100

    Change shutdown pin (lgpio didn't like using pin 5)

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -4,14 +4,15 @@ import RPi.GPIO as GPIO
 import subprocess
 import time
 
+SHUTDOWN_PIN = 27
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SHUTDOWN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    GPIO.wait_for_edge(3, GPIO.FALLING)
+    GPIO.wait_for_edge(SHUTDOWN_PIN, GPIO.FALLING)
 
     # Wait for 1 second to check if the button is still pressed
     time.sleep(1)
 
-    if not GPIO.input(3):  # If the button is still pressed after 1 second
+    if not GPIO.input(SHUTDOWN_PIN):  # If the button is still pressed after 1 second
         subprocess.call(['shutdown', '-h', 'now'], shell=False)
